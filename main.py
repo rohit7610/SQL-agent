cc5b0d37b4d506940a0c791799eefed724fea7b8
rohit7610 <rohitbishtb76@gmail.com>
2026-09-20 23:37:06 +0530

    srtreamlit ui

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import sqlite3
+import streamlit as st
 from langchain_ollama import ChatOllama
 from langchain_community.utilities import SQLDatabase
 from langchain.agents import create_agent
 from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 db = SQLDatabase.from_uri("sqlite:///Database/shop.db") # URI format for sqlite
 
@@ -18,15 +21,28 @@ agent = create_agent(
     tools = tools
 )
 
-question = input("Ask a question")
+#print(response["messages"][-1].content)
+
+prompt = ChatPromptTemplate.from_messages(
+  [
+  ("system","You are a helpful assitant that answers the user according to the sql database"),
+  ("user","Question:{question}")
+  ]
+)
+
+st.title("SQL Agent")
+input_text = st.text_input("ask the question")
+
+output_parser = StrOutputParser()
 
 response = agent.invoke({
     "messages": [
         {
-            "role": "user",
-            "content": question
+           "role": "user",
+            "content": input_text
         }
     ] 
 })
 
-print(response["messages"][-1].content)
+if input_text:
+    st.write(response["messages"][-1].content)
