4cda0c9794340cc4214f4bc0641822abb7c93b4a
rohit7610 <rohitbishtb76@gmail.com>
2026-09-20 12:57:45 +0530

    fix sql agent tool calling

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sqlite3
 from langchain_ollama import ChatOllama
 from langchain_community.utilities import SQLDatabase
-from langchain_community.agent_toolkits import create_sql_agent
+from langchain.agents import create_agent
 from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
 
 db = SQLDatabase.from_uri("sqlite:///Database/shop.db") # URI format for sqlite
@@ -11,15 +11,22 @@ print("connected databse",db.get_usable_table_names()) # check to ensure lanchai
 llm = ChatOllama(model = "qwen2.5:7b", temperature=0)
 toolkit = SQLDatabaseToolkit(db=db,llm = llm)
 
-agent = create_sql_agent(
-    llm = llm,
-    toolkit=toolkit,
-    verbose=True,
-    handle_parsing_errors=True
+tools = toolkit.get_tools()
+
+agent = create_agent(
+    model = llm,
+    tools = tools
 )
 
 question = input("Ask a question")
 
-response = agent.invoke({"input": question})
+response = agent.invoke({
+    "messages": [
+        {
+            "role": "user",
+            "content": question
+        }
+    ] 
+})
 
-print(response["output"])
+print(response["messages"][-1].content)
